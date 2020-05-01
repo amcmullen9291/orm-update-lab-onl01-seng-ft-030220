750454ce3f688042c70909582f230ea4563513ea
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 16:25:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
 def self. create_table 
   sql = <<-SQL
-  CREATE TABLE IF NOT EXIST students (
+  CREATE TABLE students (
   id INTEGER PRIMARY KEY,
   name TEXT,
   grade INTEGER
