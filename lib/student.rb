2f0ef52134795d8cbd4aa2f7bcf96ab0506d31fe
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 16:52:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ end
 
 def update
   sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
-  DB[:conn].execute(sql, self.name, self.album, self.id)
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
 end
 
 def self.create(name, grade)
