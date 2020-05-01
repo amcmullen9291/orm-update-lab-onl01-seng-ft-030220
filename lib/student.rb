8c146bcd08c0aba5f4eac2b0e57085aa4f16fb99
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 17:21:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ def self.create(name, grade)
 end
 
 def self.new_from_db(row)
-  new_student = self.new(name, grade, id)
+  new_student = self.new(name, id)
   new_student.id = row[0]
   new_student.name =  row[1]
   new_student.grade = row[2]
