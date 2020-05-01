2b264f266687ceeeca6242fbd06718cb4b417ef0
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 17:25:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,5 +59,17 @@ def self.new_from_db(row)
   new_student.grade = row[2]
 end
 
+def self.find_by_name(name)
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE name = ?
+  LIMIT 1
+  SQL
+  DB[:conn].execute(sql,name).map do |row|
+    self.new_from_db(row)
+  end.first 
+
+end 
 
 end
