e0081e75735a62308138ff53e756bdee3145c84f
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 16:33:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ def save
   SQL
   
   DB[:conn].execute(sql)
-    @id = DB[:conn].execute("Select last_insert_rowid() FROM students")
+    @id = DB[:conn].execute("Select last_insert_rowid()")
 end
 
 end
